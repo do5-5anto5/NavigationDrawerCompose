0e1422d1ef8c2296214012fcf1c2974eccfdaa20
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-07-23 20:25:46 -0300

    Add colors

--- app/src/main/java/com/example/navigationdrawercompose/ui/theme/Color.kt
+++ app/src/main/java/com/example/navigationdrawercompose/ui/theme/Color.kt
@@ -2,6 +2,18 @@ package com.example.navigationdrawercompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Light
+val PrimaryColorLight = Color(0xFFffda14)
+val OnPrimaryColorLight = Color(0xFFffda14)
+val TextColorLight = Color(0xFF000000)
+val BackgroundColorLight = Color(0xFFffffff)
+
+// Dark
+val SecondaryColorDark = Color(0xFF111b21)
+val OnSecondaryColorDark = Color(0xFF202c33)
+val TextColorDark = Color(0xFFffffff)
+val BackgroundColorDark = Color(0xFF111b21)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
